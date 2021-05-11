0ab22db56a3d63057c3ee862e963ef28447f5d36
sudhanshu1993 <49163737+sudhanshu1993@users.noreply.github.com>
2021-05-11 14:36:24 +0530

    uploaad

---
+++ resources/views/front_end/index.blade.php
@@ -0,0 +1,183 @@
+<div class="index-top">
+    <div class="slide"> 
+        <ul class="s-list">
+            <li class="s-list-pic1">
+                <a href="#">
+                    <img src="{{ asset('user/front_end/images/banner-cov.png')}}" alt="Seriously, Refrigerating These 12 Foods Will Do Harm To Your Health">
+                </a>
+            </li>
+            <li class="s-list-pic2">
+                <a href="#">
+                    <img src="{{ asset('user/front_end/images/slider1.jpg')}}" alt="Can I Use Expired Spices?">
+                        <span>
+                            Can I Use Expired Spices?
+                        </span>
+                </a>
+            </li>
+            <li class="s-list-pic3">
+                <a href="#">
+                    <img src="{{ asset('user/front_end/images/slider2.jpg')}}" alt="Why You Should Eat More Cranberries and Easy Recipes">
+                        <span>
+                            Why You Should Eat More Cranberries and Easy Recipes
+                        </span>
+                </a>
+            </li>
+            <li class="s-list-pic4">
+                <a href="#">
+                    <img src="{{ asset('user/front_end/images/slider3.jpg')}}" alt="Reasons to Indulge in Dark Chocolate">
+                        <span>
+                            Reasons to Indulge in Dark Chocolate
+                        </span>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="menu-price" id="menu-price">
+        <h2><span>Menu Price</span></h2>
+            <ul class="mp-logo">
+                <li>
+	                <a href="menu/chick-fil-a/">
+		                <img src="{{ asset('user/front_end/images/brand_logo/chick-fil-a.png')}}" alt="Chick-fil-A menu" />
+	                </a>
+                </li>
+                <li>
+                    <a href="menu/raising-canes/">
+                        <img src="{{ asset('user/front_end/images/brand_logo/raising-canes.png')}}" alt="Raising Cane's menu" />
+                    </a>
+                </li>
+                <li>
+                    <a href="menu/kfc/">
+                        <img src="i{{ asset('user/front_end/images/brand_logo/kfc.png')}}" alt="KFC menu" />
+                    </a>
+                </li>
+                <li>
+                    <a href="menu/wendys/">
+                        <img src="{{ asset('user/front_end/images/brand_logo/wendys.png')}}" alt="Wendy's menu" />
+                    </a>
+                </li>
+                <li>
+                    <a href="menu/little-caesars-pizza/">
+                        <img src="{{ asset('user/front_end/images/brand_logo/little-caesars-pizza.png')}}" alt="Little Caesars Pizza menu" />
+                    </a>
+                </li>
+                <li>
+                    <a href="menu/mcdonalds/">
+                        <img src="{{ asset('user/front_end/images/brand_logo/mcdonalds.png')}}" alt="McDonald's menu" />
+                    </a>
+                </li>
+                <li>
+                    <a href="menu/arbys/">
+                        <img src="{{ asset('user/front_end/images/brand_logo/arbys.png')}}" alt="Arby's menu" />
+                    </a>
+                </li>
+                <li>
+                    <a href="menu/chipotle-mexican-grill/">
+                        <img src="{{ asset('user/front_end/images/brand_logo/chipotle-mexican-grill.png')}}" alt="Chipotle Mexican Grill menu" />
+                    </a>
+                </li>
+                <li>
+                    <a href="menu/pizza-hut/">
+                        <img src="{{ asset('user/front_end/images/brand_logo/pizza-hut.png')}}" alt="Pizza Hut menu" />
+                    </a>
+                </li>
+
+                <li>
+                    <a href="menu/long-john-silvers/">
+                        <img src="{{ asset('user/front_end/images/brand_logo/long-john-silvers.png')}}" alt="Long John Silver's menu" />
+                    </a>
+                </li>
+                <li>
+                    <a href="menu/jack-in-the-box/">
+                        <img src="{{ asset('user/front_end/images/brand_logo/jack-in-the-box.png')}}" alt="Jack in the Box menu" />
+                    </a>
+                </li>
+
+                <li>
+                    <a href="menu/del-taco/">
+                        <img src="{{ asset('user/front_end/images/brand_logo/del-taco.png')}}" alt="Del Taco menu" />
+                    </a>
+                </li>
+                <li>
+                    <a href="menu/golden-corral/">
+                        <img src="{{ asset('user/front_end/images/brand_logo/golden-corral.png')}}" alt="Golden Corral menu" />
+                    </a>
+                </li>
+                <li>
+                    <a href="menu/jimmy-johns/">
+                        <img src="{{ asset('user/front_end/images/brand_logo/jimmy-johns.png')}}" alt="Jimmy John's menu" />
+                    </a>
+                </li>
+
+                <li>
+                    <a href="menu/steak-n-shake/">
+                        <img src="{{ asset('user/front_end/images/brand_logo/steak-n-shake.png')}}" alt="Steak 'n Shake menu" />
+                    </a>
+                </li>
+            </ul>
+    </div>
+</div>
+<div class="menu-price food-cl-list">
+    <ul class="h-hours">
+        <li>
+            <a href="donuts-near-me/">
+                <span>
+                    <img src="{{ asset('user/front_end/images/food_near_me/donuts-near-me.png')}}" alt="Donuts near me">
+                </span>
+                <h3>
+                    Donuts Near Me
+                </h3>
+            </a>
+        </li>
+        <li>
+            <a href="brunch-near-me/">
+                <span>
+                    <img src="{{ asset('user/front_end/images/food_near_me/brunch-near-me.png')}}" alt="Brunch near me">
+                </span>
+                <h3>
+                    Brunch Near Me
+                </h3>
+            </a>
+        </li>
+        <li>
+            <a href="burgers-near-me/">
+                <span>
+                    <img src="{{ asset('user/front_end/images/food_near_me/burgers-near-me.png')}}" alt="Burger near me">
+                </span>
+                <h3>
+                    Burger Near Me
+                </h3>
+            </a>
+        </li>
+        <li>
+            <a href="pizza-near-me/">
+                <span>
+                    <img src="{{ asset('user/front_end/images/food_near_me/pizza-near-me.png')}}" alt="Pizza near me">
+                </span>
+                <h3>
+                    Pizza Near Me
+                </h3>
+            </a>
+        </li>
+        <li>
+            <a href="seafood-near-me/">
+                <span>
+                    <img src="{{ asset('user/front_end/images/food_near_me/seafood-near-me.png')}}" alt="Seafood near me">
+                </span>
+                <h3>
+                    Seafood Near Me
+                </h3>
+            </a>
+        </li>
+        <li>
+            <a href="mexican-near-me/">
+                <span>
+                    <img src="{{ asset('user/front_end/images/food_near_me/mexican-near-me.png')}}" alt="Mexican near me">
+                </span>
+                <h3>
+                    Mexican Near Me
+                </h3>
+            </a>
+        </li>
+    </ul>
+</div>
+
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no,viewport-fit=cover" />
+<link rel="alternate" href="#" hreflang="en-us" />
+<title>Website </title>
+<link href="{{ asset('user/front_end/images/favicon.ico')}}" rel="shortcut icon">
+<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72×72-precomposed.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114×114-precomposed.png">
+<link rel="preload" href="{{ asset('user/front_end/css/style.css?v-0203')}}" as="style">
+<link rel="stylesheet" type="text/css" href="{{ asset('user/front_end/css/style.css?v-0203')}}" />
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&family=Rubik:wght@400;500;700&display=swap" rel="stylesheet">
+<link rel="manifest" href="{{ asset('user/front_end/js/manifest.json')}}">
+<link rel="canonical" href="#" />
+<meta name="description" content="Menu With Price: Here are up-to-date menus and prices of all fast food restaurants such as Applebee's, Burger King, Wendy's and KFC. Find a restaurant near you and order now!" />
+<meta name="keywords" content="fast food, menu, menu prices, happy hours, restaurant near me, restaurant menu, restaurant hours, restaurant locations" /> <script data-ad-client="ca-pub-6371063963738592" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" type="32d3d962bf56aa0734ff3580-text/javascript">
+</script>
+</head>
+<body id="index-body">
+
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MFFDM34"
+        height="0" width="0" style="display:none;visibility:hidden">
+    </iframe>
+</noscript>
+
+
+<div class="header" style="background-color: gray;">
+<label for="mn-mbl" class="body_btn"></label>
+<label class="mnmbl-label" for="mn-mbl"><span></span><span></span><span></span></label>
+<div class="hd-bar" style="background-color: gray;" >
+<a class="logo" href="#">
+<img src="{{ asset('user/front_end/images/icon.png')}}" alt="Menu With Price">
+</a>
+ </div>
+<div class="menu" style="background-color: gray;">
+<div class="menu_width">
+<a class="logo" href="index.htm">
+<img src="{{ asset('user/front_end/images/icon.png')}}" alt="Menu With Price">
+</a>
+<ul class="m-list">
+<li class="ml-home"><a href="index.htm" class="active" style="background-color: gray;">home</a></li>
+<li><a href="menu-and-price/index.htm" style="background-color: gray;">all menus</a></li>
+<li><a href="restaurant-near-me/index.htm" style="background-color: gray;">food near me</a></li>
+<li><a href="cuisine/index.htm" style="background-color: gray;">Cuisine</a></li>
+<li><a href="nutrition/index.htm" style="background-color: gray;">Nutrition</a></li>
+
+</ul>
+</div>
+</div>
+</div>
